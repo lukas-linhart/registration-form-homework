4caf912afbe2bd5e70b1b3735e4ccb4a587ac114
Lukáš Linhart <lukas.linhart.1981@gmail.com>
2017-04-10 08:09:23 +0200

    input fields skelet

--- src/App.js
+++ src/App.js
@@ -2,16 +2,72 @@ import React, { Component } from 'react';
 import './App.css';
 
 
+class FormField extends Component {
+  render() {
+    return (
+        <FormInput
+          name={this.props.name}
+          label={this.props.label}
+          value={this.props.value}
+        />
+    );
+  };
+}
+
+
+
+class FormInput extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <label>{this.props.label}</label>
+        </div>
+        <div>
+          <input
+            name={this.props.name}
+            value={this.props.value}
+          />
+          <div>error placeholder</div>
+        </div>
+      </div>
+    );
+  }
+}
+
 
 class RegistrationForm extends Component {
   constructor(props) {
     super(props);
+
+    // order of items will affect order of rendered form inputs
+    this.inputs = [
+      'firstName',
+      'lastName',
+      'email',
+      'address',
+      'birthdate',
+      'password1',
+      'password2'
+    ];
   }
 
   render() {
+    let formFields = [];
+    for (let fieldName of this.inputs) {
+      formFields.push(
+          <FormField
+            key={fieldName}
+            name={fieldName}
+            label={fieldName}
+            value=""
+          />
+      );
+    }
+
     return (
         <form onSubmit={this.handleSubmit}>
-          <div>Registration form</div>
+          {formFields}
           <input type="submit" value="Register" />
         </form>
     );
